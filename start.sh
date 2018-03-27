20503e9eb6491ee364e8df29105986a901bae389
Jonas Krogell <jonas@krogell.se>
2018-03-27 16:07:45 +0200

    testing with new start file

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 MYIP=$(hostname -i)
+echo "Starting bgp-injector with parameters (myip, peerip, asn):", $MYIP, $PEERIP, $ASN
 perl bgp_simple.pl -myas $ASN -myip $MYIP -peeras $ASN -peerip $PEERIP -p myroutes.txt -holdtime 300
